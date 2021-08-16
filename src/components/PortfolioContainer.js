03c4bb15c20a873f8ebaeee673a4a5fa55de43ba
Junny <tajo0215@gmail.com>
2021-08-16 11:27:39 -0700

    upload files

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  // TODO: Add a comment describing the functionality of this method
+  const renderPage = () => {
+    switch (currentPage) {
+      case "Home":
+        return <Home />;
+
+      case "About":
+        return <About />;
+
+      case "Blog":
+        return <Blog />;
+
+      default:
+        return <Contact />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* // TODO: Add a comment describing what we are passing as props */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* // TODO: Add a comment explaining what is happening on the following line */}
+      {renderPage()}
+    </div>
+  );
+}
